f86da6b2d460accb280bdbb6b688cbb81bed4191
nurssyifa <58224351+nurssyifa@users.noreply.github.com>
2019-11-26 22:05:38 +0630

    Add files via upload

---
+++ Latihan Visual/Mahasiswa.vb
@@ -0,0 +1,69 @@
+﻿Public Class Mahasiswa
+    'Deklarasi Properti
+    Public nama As String
+    Public nim As Integer
+    Public nilaiTugas As Integer
+    Public nilaiUts As Integer
+    Public nilaiUas As Integer
+    Public nilaiAkhir As Integer
+    Public nilaiHuruf As String
+
+    Public Sub New()
+        nama = ""
+        nim = 0
+        nilaiTugas = 0
+        nilaiUts = 0
+        nilaiUas = 0
+    End Sub
+
+    Public Sub InputData()
+        Console.Write("Masukkan Nama: ")
+        nama = Console.ReadLine()
+        Console.Write("Masukkan NIM: ")
+        nim = Console.ReadLine()
+        Console.Write("Masukkan Nilai Tugas: ")
+        nilaiTugas = Console.ReadLine()
+        Console.Write("Masukkan Nilai UTS: ")
+        nilaiUts = Console.ReadLine()
+        Console.Write("Masukkan Nilai UAS: ")
+        nilaiUas = Console.ReadLine()
+    End Sub
+
+    Public Sub hitungNilaiAkhir()
+        nilaiAkhir = (nilaiTugas + nilaiUts + nilaiUas) / 3
+        Console.WriteLine(nilaiAkhir)
+    End Sub
+
+    Public Sub konversiNilaiAkhir()
+
+        If nilaiAkhir <= 100 And nilaiAkhir >= 70 Then
+            nilaiHuruf = "A"
+            Console.WriteLine(nilaiHuruf)
+        ElseIf nilaiAkhir <= 69 And nilaiAkhir >= 60 Then
+            nilaiHuruf = "B"
+            Console.WriteLine(nilaiHuruf)
+        ElseIf nilaiAkhir <= 59 And nilaiAkhir >= 50 Then
+            nilaiHuruf = "C"
+            Console.WriteLine(nilaiHuruf)
+        ElseIf nilaiAkhir <= 49 And nilaiAkhir >= 0 Then
+            nilaiHuruf = "D"
+            Console.WriteLine(nilaiHuruf)
+        End If
+    End Sub
+
+    Public ReadOnly Property GetNilaiAkhir() As Double
+        Get
+            Return nilaiAkhir
+        End Get
+    End Property
+
+    Public ReadOnly Property GetNilaiHuruf() As String
+        Get
+            Return nilaiHuruf
+        End Get
+    End Property
+
+End Class
+
+
+
---
+++ Latihan Visual/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim objMahasiswa As New Mahasiswa
+
+        objMahasiswa.InputData()
+
+        Console.Clear()
+
+        Console.WriteLine("- REKAP DATA -")
+        Console.WriteLine(objMahasiswa.nama)
+        Console.Write("Nilai Akhir: ")
+        objMahasiswa.hitungNilaiAkhir()
+        Console.Write("Nilai Huruf: ")
+        objMahasiswa.konversiNilaiAkhir()
+        Console.ReadLine()
+
+    End Sub
+
+End Module
